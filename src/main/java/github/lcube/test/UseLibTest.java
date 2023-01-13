fe832681d849e720cecad5f197637d5708063343
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2023-01-13 16:25:33 +0100

    initial commit

---
+++ src/main/java/github/lcube/test/UseLibTest.java
@@ -0,0 +1,11 @@
+package github.lcube.test;
+
+import github.lcube.demo.HelloMaven;
+
+public class UseLibTest {
+
+    public static void main(String[] args) {
+        HelloMaven.run();
+    }
+
+}
